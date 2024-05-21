ee98651987069445ba8c298c20f178c3481b51be
Suman Kisku <sumankisku1@gmail.com>
2024-05-21 20:39:11 +0530

    Added voting app in projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,4 +14,11 @@ export const projects = [
         deploy_link: "https://ecommerce-react-website.netlify.app",
         keywords: ["React JS", "Context API", "Matelrial UI", "Cart Feature", "React Hot Toast"],
     },
+    {
+        title: "Web 3 Voting Website",
+        description: "A decentralized voting app(Mimicking Lok Sabha Vote 2024). Where you can vote pseudonymously. Built on polygon Amoy Testnet",
+        github_link: "https://github.com/SumanKisku/lok-sabha-2024-poll",
+        deploy_link: "https://lok-sabha-2024-poll.netlify.app/",
+        keywords: ["Solidity", "Polygon Testnet", "React JS", "Thirdweb", "Web 3", "Blockchain"],
+    },
 ];
